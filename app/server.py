f5becc4b669786d56e307e7a3aaea8c4e3afb6bc
Bryan6aero <33768965+Bryan6aero@users.noreply.github.com>
2019-11-12 14:30:07 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1ebo8D-HVJo_xchsWCWi3He_t5E2Hnx4F'
 export_file_name = 'export.pkl'
 
-classes = ['photorealism','impressionism','futurism','suprematism','surrealism','orphism','tonalism','cubism','expressionism','fauvism']
+classes = ['photorealism','impressionism','futurism','suprematism','surrealism','orphism','tonalism','cubism','expressionism','fauvism','realism']
 path = Path(__file__).parent
 
 app = Starlette()
