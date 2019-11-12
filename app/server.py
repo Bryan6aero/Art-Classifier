cca4d5de8d09aed8acdd4b0af29b2a477070faee
Bryan6aero <33768965+Bryan6aero@users.noreply.github.com>
2019-11-11 22:18:14 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1ebo8D-HVJo_xchsWCWi3He_t5E2Hnx4F'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['photorealism','impressionism','futurism','suprematism','surrealism','orphism','tonalism','cubism','expressionism','fauvism']
 path = Path(__file__).parent
 
 app = Starlette()
